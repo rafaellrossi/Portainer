41c60e3c9ad19b4f387f7cfbd7c1364162e6eee6
Rafael Rossi <rafaelrossij@gmail.com>
2024-09-16 21:02:37 -0300

    Update

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # Portainer
 
+#### Instalação no Linux (Recomendado: Ubuntu)
+
 ```bash
-wget https://raw.githubusercontent.com/rafaellrossi/Portainer/main/portainer.sh && chmod +x portainer.sh && ./portainer.sh
+sudo apt update && sudo apt install -y curl
+```
 
-
+```bash
+curl -fsSL https://raw.githubusercontent.com/rafaellrossi/Portainer/main/portainer.sh | bash
+```
--- portainer.sh
+++ portainer.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-sudo apt update && sudo apt upgrade -y
-sudo apt install -y docker.io docker-compose 
+sudo apt update 
+sudo apt upgrade -y
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin 
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo docker volume create portainer_data
