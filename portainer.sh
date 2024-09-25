ea2f847b82a229e9c0e72427482d2d9452cc6554
rafaellrossi <91340426+rafaellrossi@users.noreply.github.com>
2024-09-25 18:25:00 -0300

    Update portainer.sh

--- portainer.sh
+++ portainer.sh
@@ -14,5 +14,7 @@ sudo sh get-docker.sh
 sudo docker volume create portainer_data
 sudo docker run -d -p 8000:8000 -p 9000:9000 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 
+echo ""
 echo "Pontainer agora está instalado e em execução."
-echo "Você pode acessá-lo em http://seu_url:9000"
+echo "Você pode acessá-lo em http://<URL>:9000"
+echo ""
