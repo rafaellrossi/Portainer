ed9b7efdf07e1bc5f6e212b2ce4b74ff39fb6490
rafaellrossi <91340426+rafaellrossi@users.noreply.github.com>
2024-09-25 18:32:37 -0300

    Update portainer.sh

--- portainer.sh
+++ portainer.sh
@@ -5,12 +5,6 @@ sudo apt upgrade -y
 sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
-# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-# sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-# sudo apt update
-# sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin 
-# sudo systemctl start docker
-# sudo systemctl enable docker
 sudo docker volume create portainer_data
 sudo docker run -d -p 8000:8000 -p 9000:9000 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 
